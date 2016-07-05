3c7d12c74e46433909e214ed51564b0fc5ff7f32
dbrown <davidbrown772255@gmail.com>
2016-07-04 20:03:32 -0400

    more jokes with random joke selection

--- app/js/rosTopicListener.js
+++ app/js/rosTopicListener.js
@@ -3,21 +3,18 @@ const TELL_JOKE = 'TELL_JOKE';
 // https://bl.ocks.org/mbostock/5872848
 var dispatch = d3.dispatch("speak");
 
-var myStartCallback = function myStartCallback() {
-    console.log('in my start callback');
+var startJoke = function() {
     var duration = 10;
     dispatch.speak({
         "duration": duration
     });
 }
 
-var myEndCallback = function() {
-    console.log('in my end callback');
-    document.getElementsByClassName("repeatQuestion")[0].click();
+var startRepeatJoke = function() {
+    document.getElementsByClassName("repeatJoke")[0].click();
 }
 
-var myEndCallback2 = function() {
-    console.log('in my end callback2');
+var startPunchLine = function() {
     document.getElementsByClassName("punchLine")[0].click();
     var duration = 8;
     dispatch.speak({
@@ -25,8 +22,7 @@ var myEndCallback2 = function() {
     });
 }
 
-var myEndCallback3 = function() {
-    console.log('in my end callback3');
+var rimShot = function() {
     document.getElementById("rimShot").play();
     // setTimeout(function() {
     //     document.getElementById("applause").play();
@@ -69,8 +65,21 @@ var lastDate = new Date((new Date()).getTime() - 10000);
 
 listener.subscribe(function(message) {
     console.log('Received message on ' + listener.name + ': ' + message.data);
+    var voiceTypeMale = '"UK English Male",';
+    var voiceTypeFemale = '"UK English Female",';
     if (message.data === TELL_JOKE) {
-        document.getElementsByClassName("jokeOne")[0].click();
+        var nextJokeIndex = Math.floor((Math.random() * data.jokes.length));
+        var onClickJoke = 'responsiveVoice.speak("' + data.jokes[nextJokeIndex].joke + '", ' + voiceTypeMale +
+        '{rate: 1, onstart: startJoke, onend: startRepeatJoke});';
+        var onClickRepeatJoke = 'responsiveVoice.speak("' +  data.jokes[nextJokeIndex].repeatJoke + '", ' + voiceTypeFemale +
+        '{onend: startPunchLine});';
+        var onClickPunchLine = 'responsiveVoice.speak("' +  data.jokes[nextJokeIndex].punchLine + '", ' + voiceTypeMale +
+        '{onend: rimShot});';
+
+        document.getElementsByClassName("joke")[0].setAttribute("onclick", onClickJoke);
+        document.getElementsByClassName("repeatjoke")[0].setAttribute("onclick", onClickRepeatJoke);
+        document.getElementsByClassName("punchLine")[0].setAttribute("onclick", onClickPunchLine);
+        document.getElementsByClassName("joke")[0].click();
     }
 
     //listener.unsubscribe();
