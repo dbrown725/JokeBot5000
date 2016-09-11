96fbbe518d2a03561cef0653adc9871dd111cc5a
dbrown <davidbrown772255@gmail.com>
2016-09-11 15:31:03 -0400

    moved jokes to their own file

---
+++ app/js/jokes.js
@@ -0,0 +1,119 @@
+var data = {
+      jokes: [{
+        "joke": "What did the finger say to the thumb?",
+        "repeatJoke": "I do not know what did the finger say to the thumb?",
+        "punchLine": "I am in glove with you"
+    }, {
+        "joke": "What happens to a frog's car when it breaks down?",
+        "repeatJoke": "I do not know what happens to a frog's car when it breaks down?",
+        "punchLine": "It gets toad away."
+    }, {
+        "joke": "What did the duck say when he bought lipstick?",
+        "repeatJoke": "I do not know what did the duck say when he bought lipstick?",
+        "punchLine": "Put it on my bill."
+    }, {
+        "joke": "What do you call a pig that does karate?",
+        "repeatJoke": "I do not know What do you call a pig that does karate?",
+        "punchLine": " A pork chop."
+    }, {
+        "joke": "What starts with E, ends with E, and has only 1 letter in it?",
+        "repeatJoke": "I do not know what starts with E, ends with E, and has only 1 letter in it?",
+        "punchLine": "Envelope. "
+    }, {
+        "joke": "What do computers eat for a snack?",
+        "repeatJoke": "I do not know what do computers eat for a snack?",
+        "punchLine": "Microchips."
+    }, {
+        "joke": "How do trees access the internet?",
+        "repeatJoke": "I do not know how do trees access the internet?",
+        "punchLine": "They log in."
+    }, {
+        "joke": "What is the tallest building in the entire world? ",
+        "repeatJoke": "I do not know what is the tallest building in the entire world?",
+        "punchLine": "The library, because it has so many stories."
+    }, {
+        "joke": "What nails do carpenters hate to hit?",
+        "repeatJoke": "I do not know what nails do carpenters hate to hit?",
+        "punchLine": "Fingernails."
+    }, {
+        "joke": "Why couldn't the leopard play hide and seek? ",
+        "repeatJoke": "I do not know why couldn't the leopard play hide and seek? ",
+        "punchLine": "Because he was always spotted."
+    }, {
+        "joke": "Why is the barn so noisy? ",
+        "repeatJoke": "I do not know why is the barn so noisy?",
+        "punchLine": "Because the cows have horns."
+    }, {
+        "joke": "What did the banana say to the doctor?",
+        "repeatJoke": "I do not know what did the banana say to the doctor?",
+        "punchLine": "I'm not peeling well."
+    }, {
+        "joke": "What has more lives than a cat?",
+        "repeatJoke": "I do not know what has more lives than a cat?",
+        "punchLine": "A frog because it croaks every night. ."
+    }, {
+        "joke": "What stays in one corner but travels around the world?",
+        "repeatJoke": "I do not know What stays in one corner but travels around the world?",
+        "punchLine": "A stamp."
+    }, {
+        "joke": "Where do pencils go for vacation?",
+        "repeatJoke": "I do not know Where do pencils go for vacation?",
+        "punchLine": "Pencil-vania."
+    }, {
+        "joke": "What did the mushroom say to the fungus?",
+        "repeatJoke": "I do not know What did the mushroom say to the fungus?",
+        "punchLine": "You're a fun guy."
+    }, {
+        "joke": "What's the difference between a guitar and a fish?",
+        "repeatJoke": "I do not know What's the difference between a guitar and a fish?",
+        "punchLine": "You can't tuna fish"
+    }, {
+        "joke": "What do lawyers wear to court?",
+        "repeatJoke": "I do not know What do lawyers wear to court?",
+        "punchLine": "Lawsuits!"
+    }, {
+        "joke": "Why does a chicken coop have two doors?",
+        "repeatJoke": "I do not know Why does a chicken coop have two doors?",
+        "punchLine": "If it had four, it would be a chicken sedan."
+    }, {
+        "joke": "What do you call a parade of rabbits hopping backwards?",
+        "repeatJoke": "I do not know What do you call a parade of rabbits hopping backwards?",
+        "punchLine": "a receding hare-line."
+    }, {
+        "joke": "What do you call an old snowman?",
+        "repeatJoke": "I do not know What do you call an old snowman?",
+        "punchLine": "Water!"
+    }, {
+        "joke": "What’s the difference between a cat and a comma?",
+        "repeatJoke": "I do not know What’s the difference between a cat and a comma?",
+        "punchLine": "A cat has claws at the end of paws; A comma is a pause at the end of a clause."
+    }, {
+        "joke": "How do you know when the moon has enough to eat?",
+        "repeatJoke": "I do not know How do you know when the moon has enough to eat?",
+        "punchLine": "When it’s full."
+    }, {
+        "joke": "What did the judge ask when he went to the dentist?",
+        "repeatJoke": "I do not know What did the judge ask when he went to the dentist?",
+        "punchLine": "Do you swear to pull the tooth, the whole tooth and nothing but the tooth?"
+    }, {
+        "joke": "Why did the chicken cross the playground?",
+        "repeatJoke": "I do not know Why did the chicken cross the playground?",
+        "punchLine": "To get to the other slide"
+    } , {
+        "joke": "How come oysters never donate to charity?",
+        "repeatJoke": "I do not know How come oysters never donate to charity?",
+        "punchLine": "Because they are shellfish."
+    }, {
+        "joke": "What kind of key opens a banana?",
+        "repeatJoke": "I do not know What kind of key opens a banana?",
+        "punchLine": "A monkey."
+    }, {
+        "joke": "Why do the French eat snails?",
+        "repeatJoke": "I do not know Why do the French eat snails?",
+        "punchLine": "They do not like fast food."
+    }, {
+        "joke": "Why did the dinosaur cross the road?",
+        "repeatJoke": "I do not know Why did the dinosaur cross the road?",
+        "punchLine": "Chickens did not exist yet."
+    }]
+};
