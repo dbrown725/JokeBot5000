b403e9090e87d017798e94225612c20123304e00
dbrown <davidbrown772255@gmail.com>
2016-09-13 11:04:44 -0400

    changed api key and location

--- app/js/config.js
+++ app/js/config.js
@@ -1,6 +1,6 @@
 //Obtain weather api key by registering at https://www.wunderground.com/weather/api/
 var config = {
-  weatherAPIKey: '845b49d61567f9ef',
-  weatherCity: 'Sykesville',
-  weatherState: 'MD',
+  weatherAPIKey: '<your api key here>',
+  weatherCity: 'Rosslyn',
+  weatherState: 'VA',
 }
